d036d853471faff649e48cb2369a56b985e8afda
MohamedRagabJr <muhamidragab201020@gmail.com>
2026-04-19 16:13:24 +0200

    ed

--- src/app/api/getWishlist.ts
+++ src/app/api/getWishlist.ts
@@ -1,15 +1,23 @@
+"use server";
+
 import { getMyToken } from "./getMyToken";
 
 export async function getUserWishlist() {
   const token = await getMyToken();
 
+  if (!token) {
+    return { data: [], count: 0, status: "unauthorized" };
+  }
+
   try {
     const res = await fetch(
       "https://ecommerce.routemisr.com/api/v1/wishlist",
       {
+        method: "GET",
         headers: {
           token: token as string
-        }
+        },
+        cache: "no-store"
       }
     );
 
